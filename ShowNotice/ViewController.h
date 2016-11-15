24470969ae7de33d373c293c1642461854ac2b51
xialinWang <oukobayashi@wangxiaolindeMacBook-Pro.local>
2016-11-15 16:26:49 +0800

    Initial Commit

---
+++ ShowNotice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowNotice
+//
+//  Created by 王小林 on 2016/11/15.
+//  Copyright © 2016年 王小林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShowNotice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShowNotice
+//
+//  Created by 王小林 on 2016/11/15.
+//  Copyright © 2016年 王小林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
